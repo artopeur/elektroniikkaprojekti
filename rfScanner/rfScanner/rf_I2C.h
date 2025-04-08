ed0acdaec5f502674c938ec9aa3ea03fae160a92
artopeur <arto.peurasaari@gmail.com>
2025-04-08 14:56:51 +0300

    framework for i2c

---
+++ rfScanner/rfScanner/rf_I2C.h
@@ -0,0 +1,30 @@
+
+#ifndef RF_I2C_H
+#define RF_I2C_H
+
+#include <stdint.h>
+
+void initI2C();
+void start_transmission();
+void stop_transmission();
+void command_lcd(unsigned char);
+void write_i2c(uint16_t);
+
+
+void initI2C() {
+
+}
+void start_transmission() {
+
+}
+void stop_transmission() {
+
+}
+void command_lcd(unsigned char data) {
+
+}
+void write_i2c(uint16_t data) {
+
+}
+
+#endif
