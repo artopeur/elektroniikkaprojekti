777635aa180c23c1646efa3f1bf5921264c1e2d8
artopeur <arto.peurasaari@gmail.com>
2025-04-17 12:53:22 +0300

    testing.

--- rfScanner/rfScanner/main.c
+++ rfScanner/rfScanner/main.c
@@ -59,17 +59,18 @@ int run(void) {
 	init_timer3(); //stops interrupts before setting timer3, enables interrupts after that.
 	//test_delay(10);
 
-	initI2C();
-	initDisp();
+	//initI2C();
+	//initDisp();
 	test_delay(100);
 
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1);
     while(1)
     {
 		#ifdef ARDUINO
-			Serial.println("Testing");
-			test_delay(100);
+			//Serial.println("Testing");
+			//test_delay(100);
 		#endif
-		//*
+		/*
 		unsigned char buffer[20] = "data:";
 		unsigned char measure[20] = "";
 		volatile float data = 0.1;
@@ -77,7 +78,7 @@ int run(void) {
 		clearScreen();
 		//*
 		//setText(1,buffer);
-		//*
+		/*
 		for (uint8_t i = 0; i<50;i++) {
 			if(i < 25) {
 				data = data + 0.1;
@@ -108,7 +109,9 @@ int run(void) {
 		float voltage= get_input_voltage(0);
 		float voltage2= get_input_voltage(1);
 		uint16_t rfvalue = adc_read(2);
-	
+		#ifdef ARDUINO
+			Serial.println(voltage);
+		#endif
 		if(voltage>2.1)
 		{
 		  if(duty_cycle<100)duty_cycle+=1;
@@ -117,15 +120,18 @@ int run(void) {
 		{
 		  if(duty_cycle>0)duty_cycle-=1;
 		}
-		if(voltage2>3.2)
+		
+		//*
+		if(voltage2>2.1)
 		{
 		  if(duty_cycle2<100)duty_cycle2-=1;
 		}
-		if(voltage2<3.2)
+		if(voltage2<2.1)
 		{
 		  if(duty_cycle2>0)duty_cycle2+=1;
 		}
-		
+		//*/
+		/*
 		float rfvolts = measurement(rfvalue, voltage); // tests previous value to previous measurement and if it is larger, returns that, otherwise returns previous highest value.
 		if(rfvolts == rfvalue) {
 			// no need to update display
--- rfScanner/rfScanner/rf_adc.h
+++ rfScanner/rfScanner/rf_adc.h
@@ -16,16 +16,16 @@ uint16_t adc_read(uint8_t);
 float get_input_voltage(uint8_t);
 
 void timer_init() {
-  uint16_t top_value=49;
+  uint16_t top_value=255;
   cli();
-  DDRB|= (1<<PB1) | (1<<PB2);
+  DDRB|= (1<<PB5); //| (1<<PB6);
   TCCR1A= (1<<COM1A1)| (1<<COM1B1) |(1<<WGM11);
-  TCCR1B= (1<<WGM13)|(1<<WGM12) | (1<<CS10);
+  TCCR1B= (1<<WGM13)|(1<<WGM12) | (1<<CS11);
 
   ICR1= top_value;
 
   OCR1A= (ICR1*duty_cycle) / 100;
-  OCR1B = (ICR1 * duty_cycle2) / 100;
+  //OCR1B = (ICR1 * duty_cycle2) / 100;
   TIMSK1 |= (1<<OCIE1A);
 
   sei(); // Enable global interrupts
@@ -38,12 +38,13 @@ void adc_init() {
 }
 
 uint16_t adc_read(uint8_t pin) {
-
+  cli();
   pin &= 0x0F;
   ADMUX = (ADMUX & 0xF0) | pin;
   ADMUX |= (1 << REFS1) | (1 << REFS0); 
   ADCSRA |= (1 << ADSC);  
   while (ADCSRA & (1 << ADSC));
+  sei();
   return ADC;  
 	
 }
@@ -51,7 +52,7 @@ uint16_t adc_read(uint8_t pin) {
 float get_input_voltage(uint8_t pin) {
 	uint16_t adc_value = adc_read(pin);  // Read ADC channel 0
 	float voltage = adc_value *  (1.1/ 1023.0);  // Convert ADC to voltage
-	return voltage * 10;  // Scale back to input voltage
+	return (120.99/21.99)*voltage;  // Scale back to input voltage
 }
 
 #endif
