0e5858400e708000ae524a1491bbebea942c70b7
artopeur <arto.peurasaari@gmail.com>
2025-04-23 08:44:02 +0300

    flash.cmd fixed and applied.

--- flash.cmd
+++ flash.cmd
@@ -8,11 +8,15 @@ set comport=%1
 if [%2] == [] goto skip
 set file=%2
 echo .\avrdude\avrdude.exe -c arduino -p m328pb -P %comport% -b 19200 -U flash:w:%file%:i
+pause
+.\avrdude\avrdude.exe -c arduino -p m328pb -P %comport% -b 19200 -U flash:w:%file%:i
 goto EOF
 
 :skip
 echo using default debug folder
-echo .\avrdude\avrdude.exe -c arduino -p m328pb -P COM5 -b 19200 -e -U flash:w:.\rfScanner\rfScanner\Debug\rfScanner.hex:i
+echo .\avrdude\avrdude.exe -c arduino -p m328pb -P %comport% -b 19200 -e -U flash:w:.\rfScanner\rfScanner\Debug\rfScanner.hex:i
+pause
+.\avrdude\avrdude.exe -c arduino -p m328pb -P %comport% -b 19200 -e -U flash:w:.\rfScanner\rfScanner\Debug\rfScanner.hex:i
 goto EOF
 :error
 echo "no comport set."
